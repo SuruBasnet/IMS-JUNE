75c25fa45b2f8deaf7dd29ca465e00aa88b8068b
Suru Basnet <surubasnet@gmail.com>
2025-07-22 08:43:57 +0545

    changes

---
+++ base/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from .models import ProductType
+from rest_framework.viewsets import ModelViewSet
+from .serializers import ProductTypeSerializer
+
+# Create your views here.
+class ProductTypeApiView(ModelViewSet):
+    queryset = ProductType.objects.all()
+    serializer_class = ProductTypeSerializer
+
+    
